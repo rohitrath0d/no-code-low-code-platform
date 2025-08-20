f2f6db4902cb15d2f59b29706041a33aef9eb50c
Rohit <rohit.rthd.04@gmail.com>
2025-08-21 01:55:29 +0530

    env var config change

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -7,6 +7,9 @@ load_dotenv()
 
 DATABASE_URL= os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+  raise RuntimeError("❌ GEMINI_API_KEY environment variable is not set")
+
 # creating SQLModel engine
 engine = create_engine(DATABASE_URL, echo=True)     #echo=True shows SQL logs
 
