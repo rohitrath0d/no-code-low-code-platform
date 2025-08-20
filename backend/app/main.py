49a0b024f61081243649478fd66f33dae75c994d
Rohit <rohit.rthd.04@gmail.com>
2025-08-21 02:06:14 +0530

    error fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -72,7 +72,7 @@ app.add_middleware(
     allow_credentials=True,
     # allow_methods=["*"],
     # allow_headers=["*"],
-    allow_methods = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+    allow_methods = ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers = ["Content-Type", "Authorization"]
 )
 
